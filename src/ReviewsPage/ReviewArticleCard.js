4831d0cb1b2df890bcac6a01ccb1563757a94ec6
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2024-05-19 22:01:14 -0400

    22

--- src/ReviewsPage/ReviewArticleCard.js
+++ src/ReviewsPage/ReviewArticleCard.js
@@ -1,23 +1,22 @@
-// ReviewArticleCard.js
-
 import React from 'react';
 import './ReviewArticleCard.css'; // Import CSS file for styling
 import { Link } from 'react-router-dom'; // Import Link
 
 const ReviewArticleCard = ({ date, title, imageUrl, path }) => {
-    return (
-      <div className="article-card-review">
+  return (
+    <div className="article-card-review">
+      <div className="article-image-wrapper-review">
         <img src={imageUrl} alt={title} className="article-image-review" />
-        <div className="article-content">
-          <div className="article-date" style={{  marginTop: '1rem',fontSize: '16px' }}>{date}</div>
-          <div className="article-title" style={{  marginTop: '1rem',fontSize: '20px' }}>{title}</div>
-          <Link to={path} className="read-more-link">
-            <button className="read-more" style={{ marginTop: '1rem',fontSize: '14px' }}>Read Here</button>
-          </Link>
-        </div>
       </div>
-    );
-  };
-  
+      <div className="article-content">
+        <div className="article-date">{date}</div>
+        <div className="article-title">{title}</div>
+        <Link to={path} className="read-more-link">
+          <button className="read-more">Read Here</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default ReviewArticleCard;
--- src/ReviewsPage/ReviewArticleList.js
+++ src/ReviewsPage/ReviewArticleList.js
@@ -8,47 +8,47 @@ const image3 = `${process.env.PUBLIC_URL}/Images/Articles/HalfetiReview/HalfetiT
 const image4 = `${process.env.PUBLIC_URL}/Images/Articles/ULIKEREVIEW/ULIKETHUMB.png`;
 
 const articles = [
-    {
-        date: '01/15/2024',
-        title: 'Jicky // Guerlain',
-        image: image1,
-        path: '/jickyreview',
-    },
-    {
-        date: '02/01/2024',
-        title: 'Bonbon // Viktor & Rolf',
-        image: image2,
-        path: '/bonbonreview',
-    },
-    {
-        date: '03/01/2024',
-        title: "Halfeti // Penhaligon's",
-        image: image3,
-        path: '/halfetireview',
-    },
-    {
-        date: '04/01/2024',
-        title: "Painless Hair Removal // ULIKE",
-        image: image4,
-        path: '/ulikereview',
-    },
-    // Add more articles as needed
+  {
+    date: '01/15/2024',
+    title: 'Jicky // Guerlain',
+    image: image1,
+    path: '/jickyreview',
+  },
+  {
+    date: '02/01/2024',
+    title: 'Bonbon // Viktor & Rolf',
+    image: image2,
+    path: '/bonbonreview',
+  },
+  {
+    date: '03/01/2024',
+    title: "Halfeti // Penhaligon's",
+    image: image3,
+    path: '/halfetireview',
+  },
+  {
+    date: '04/01/2024',
+    title: "Painless Hair Removal // ULIKE",
+    image: image4,
+    path: '/ulikereview',
+  },
+  // Add more articles as needed
 ];
 
 const ReviewArticleList = () => {
-    return (
-        <div className="article-list-reviews">
-            {articles.map((article, index) => (
-                <ReviewArticleCard
-                    key={index}
-                    date={article.date}
-                    title={article.title}
-                    imageUrl={article.image}
-                    path={article.path}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="article-list-reviews">
+      {articles.map((article, index) => (
+        <ReviewArticleCard
+          key={index}
+          date={article.date}
+          title={article.title}
+          imageUrl={article.image}
+          path={article.path}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ReviewArticleList;
