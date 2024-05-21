ac3aa8f2ee2e90d1c6869a1f696f478c7efae57a
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2024-05-20 20:09:15 -0400

    11

--- src/Quiz/Quiz2.js
+++ src/Quiz/Quiz2.js
@@ -95,7 +95,7 @@ const Quiz2 = () => {
       options: [
         { text: "Sweet", value: "1", backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz2/sweet.jpeg` }, // Bunny
         { text: "Elegant", value: "2", backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz2/elegant.jpeg` }, // Deer
-        { text: "Adventurous", value: "3", backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz2/adventurous.jpeg` }, // Fox
+        { text: "Adventurous", value: "3", backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz2/adventurous.jpg` }, // Fox
         { text: "Confident", value: "4", backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz2/confident.jpeg` }, // Cat
       ],
     },
--- src/Quiz/QuizHomePage.js
+++ src/Quiz/QuizHomePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Grid, Paper } from '@mui/material';
 import TextReveal from '../Components/TextReveal';
-import RotatingText from '../Navigation/AppBar/RotatingText';
 import Quiz2 from './Quiz2';
 
 const QuizHomepage = () => {
@@ -14,21 +13,16 @@ const QuizHomepage = () => {
             
                 
                     <TextReveal text='WHAT KIND OF PRETTY ARE YOU?' />
-                    <Paper elevation={3} sx={{ backgroundColor: 'white', fontFamily: 'GFS Didot', padding: '1rem' }}>
-                    <RotatingText />
-                    </Paper>
+                
                 <Grid item xs={12}>
    
         </Grid>
                 <Quiz2 />
             </div>
 
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <div style={{ width: '50%' }}>
-                    <TextReveal text='ARE YOU EVEN A SWIFTIE?' />
-                </div>
+       
               
-            </div>
+          
         </div>
     );
 };
