ae9308d84ad1cd8e46a6d2c43bd411f0bedb1bdf
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2024-05-30 11:40:25 -0400

    2

--- src/Components/ResponsiveIframe.js
+++ src/Components/ResponsiveIframe.js
@@ -28,20 +28,25 @@ const ResponsiveIframe = ({ src, title }) => {
   };
 
   return (
-    <iframe
-      width="100%"
-      height="500px"
-      title={title}
-      src={src}
-      scrolling={allowScrolling ? 'yes' : 'no'}
-      style={{
-        border: 'none',
-        pointerEvents: iframeInteraction ? 'auto' : 'none',
-        overflow: allowScrolling ? 'auto' : 'hidden', // Ensure overflow is managed correctly
-      }}
-      onPointerEnter={handleIframePointerEnter}
-      onPointerLeave={handleIframePointerLeave}
-    ></iframe>
+    <div style={{ position: 'relative', paddingTop: '56.25%' }}>
+      <iframe
+        title={title}
+        src={src}
+        scrolling={allowScrolling ? 'yes' : 'no'}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          pointerEvents: iframeInteraction ? 'auto' : 'none',
+          overflow: allowScrolling ? 'auto' : 'hidden', // Ensure overflow is managed correctly
+        }}
+        onPointerEnter={handleIframePointerEnter}
+        onPointerLeave={handleIframePointerLeave}
+      ></iframe>
+    </div>
   );
 };
 
