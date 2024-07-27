fb98cd4634e5bf4884211bd6a9571e344ef381c1
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-07-26 20:36:10 -0400

    43

--- src/Home/ImageGridTutorials.js
+++ src/Home/ImageGridTutorials.js
@@ -12,10 +12,6 @@ const image6 = `${process.env.PUBLIC_URL}/Images/Home/HomePage6.jpg`;
 const image7 = `${process.env.PUBLIC_URL}/Images/Home/HomePage7.jpg`;
 const image8 = `${process.env.PUBLIC_URL}/Images/Home/HomePage8.jpg`;
 
-
-
-
-
 const itemData = [
   {
     img: image1,
@@ -27,7 +23,7 @@ const itemData = [
   },
   {
     img: image3,
-    title: 'Jackie Wyers Latte Makeup Headhot',
+    title: 'Jackie Wyers Latte Makeup Headshot',
   },
   {
     img: image4,
@@ -35,33 +31,28 @@ const itemData = [
   },
   {
     img: image5,
-    title:'Jackie Wyers New York City',
+    title: 'Jackie Wyers New York City',
   },
   {
     img: image6,
-    title:'Jackie Wyers Barbie Margot',
+    title: 'Jackie Wyers Barbie Margot',
   },
-    {
-        img: image7,
-        title: 'Jackie Wyers in San Diego',
-    },
-    {
-        img: image8,
-        title:"Jackie Wyers Penhaligon's Campaign",
-    },
-
-  // ... repeat for each image ...
-
+  {
+    img: image7,
+    title: 'Jackie Wyers in San Diego',
+  },
+  {
+    img: image8,
+    title: "Jackie Wyers Penhaligon's Campaign",
+  },
 ];
 
 export default function QuiltedImageList1() {
   return (
     <ImageList
-      sx={{ width: '100%', height: '55vh',  boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.9}' }}
+      sx={{ width: '100%', height: '55vh', boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.9)' }}
       variant="quilted"
       cols={4}
-   
-      
     >
       {itemData.map((item, index) => (
         <ImageListItem key={index} cols={item.cols || 1} rows={item.rows || 1}>
