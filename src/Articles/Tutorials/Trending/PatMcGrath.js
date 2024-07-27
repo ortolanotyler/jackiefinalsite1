554ca62432114b3d26b7d638e4e58b094506eb55
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-07-26 20:21:17 -0400

    43

--- src/Articles/Tutorials/Trending/PatMcGrath.js
+++ src/Articles/Tutorials/Trending/PatMcGrath.js
@@ -58,6 +58,20 @@ const PatMcGrathPost = () => {
 
   const blogContent = (
     <div className="container">
+      <Helmet>
+                <title>Pat McGrath's Porcelain Doll</title>
+                <meta name="description" content="A detailed tutorial of how to properly execute Pat's iconic makeup trend, with images and video tutorials linked" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-RT6GR7JXYG"></script>
+                <script>
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-RT6GR7JXYG');
+                    `}
+                </script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4660168246825318" crossorigin="anonymous"></script>
+            </Helmet>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25vh' }}>
         <a href="/trends" style={{ fontFamily: 'Arapey', fontSize: '1.25em', color: 'black', textDecoration: 'none' }}>
           BEAUTY // TRENDS
