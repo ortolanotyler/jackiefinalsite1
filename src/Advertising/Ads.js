81d6c5842f181872c0562b32db48c5e6f30e6632
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-07-27 23:21:45 -0400

    43

--- src/Advertising/Ads.js
+++ src/Advertising/Ads.js
@@ -16,16 +16,18 @@ const AdSenseAd = () => {
       }
     };
 
-    if (!window.adsbygoogle) {
-      const script = document.createElement('script');
-      script.async = true;
-      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4660168246825318';
-      script.crossOrigin = 'anonymous';
-      script.onload = loadAd;
-      document.body.appendChild(script);
-    } else {
-      loadAd();
-    }
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4660168246825318';
+    script.crossOrigin = 'anonymous';
+    script.onload = loadAd;
+    document.body.appendChild(script);
+
+    return () => {
+      if (script) {
+        document.body.removeChild(script);
+      }
+    };
   }, []);
 
   return (
@@ -33,10 +35,10 @@ const AdSenseAd = () => {
       <ins
         ref={adRef}
         className="adsbygoogle"
-        style={{ display:'block', textAlign: 'center' }}
+        style={{ display: 'block', textAlign: 'center' }}
         data-ad-client="ca-pub-4660168246825318"
         data-ad-slot="5895624541"
-        data-ad-format="horizontal"
+        data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
     </div>
