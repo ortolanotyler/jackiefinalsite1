ec61fc7474c55ed94fe91dc2c66d2417fcd4373c
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-07-28 20:25:44 -0400

    43

--- src/Quiz/Quiz3.js
+++ src/Quiz/Quiz3.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Quiz1.css';
 import { Helmet } from 'react-helmet';
@@ -9,6 +9,7 @@ const Quiz3 = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState([]);
   const [showResults, setShowResults] = useState(false);
+  const [buttonHeight, setButtonHeight] = useState('auto');
 
   const quizQuestions = [
     {
@@ -18,24 +19,30 @@ const Quiz3 = () => {
         { text: "A historic castle 🏰", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/2button.jpg` },
         { text: "Glamorous art deco 🎨", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/3button.jpg` },
         { text: "Upscale beach wedding 🏖️", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/4button.jpg` },
+        { text: "Luxurious ballroom 💃", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/a.jpeg` },
+        { text: "Garden Party 🌸", value: 7, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/b.jpeg` },
       ],
     },
     {
       question: "Which dresses are you most drawn to?",
       options: [
-        { text: "Romantic and whimsical 💕", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/5button.jpg` },
-        { text: "Classic and regal 👑", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/6button.jpg` },
-        { text: "Glamorous and timeless 🎩", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/7button.jpg` },
+        { text: "Romantic and whimsical 💕", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/6button.jpg` },
+        { text: "Princesscore 👑", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/c.jpeg` },
+        { text: "Simple & Timeless 🎩", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/d.jpeg` },
         { text: "Elegant and sophisticated 💍", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/8button.jpg` },
+        { text: "Always Appropriate & Chic 👗", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/e.jpeg` },
+        { text: "Showgirl Glam 💎", value: 6, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/f.jpeg` },
       ],
     },
     {
       question: "Which accessory would complete your bridal look?",
       options: [
         { text: "A delicate flower crown 🥀", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/9button.jpg` },
-        { text: "A sparkling tiara 👸🏻", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/10button.jpg` },
-        { text: "Vintage pearls ✨", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/11button.jpg` },
+        { text: "Handmade flower pins 🌸", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/g.jpeg` },
+        { text: "Diamond Studded Hair Pins 💎", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/h.jpeg` },
         { text: "A chic veil 👰🏻‍♀️", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/12button.jpg` },
+        { text: "A sparkling tiara 👸🏻", value: 6, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/10button.jpg` },
+        { text: "Heirloom Earrings ✨", value: 7, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/i.jpeg` },
       ],
     },
     {
@@ -43,38 +50,60 @@ const Quiz3 = () => {
       options: [
         { text: "Earthy Wildflowers 🌼", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/13button.jpg` },
         { text: "Classic White & Green 💚", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/14button.jpg` },
-        { text: "Glamorous Bouquet 💐", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/15button.jpg` },
-        { text: "Luxurious Bouquet 🌹", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/16button.jpg` },
+        { text: "Romantic Bouquet 🥀", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/j.jpeg` },
+        { text: "Red Roses 🥀", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/k.jpeg` },
+        { text: "White Roses 🤍", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/l.jpeg` },
+        { text: "Luxurious Bouquet 🪶", value: 6, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/15button.jpg` },
       ],
     },
     {
       question: "Which part of your body do you want to feature most?",
       options: [
-        { text: "Bare Shoulders", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/17button.jpg` },
-        { text: "Tiny Waist", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/18button.jpg` },
-        { text: "Curvy Hips", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/19button.jpg` },
-        { text: "Bare Back", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/20button.jpg` },
+        { text: "Bare Shoulders 👗", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/17button.jpg` },
+        { text: "Tiny Waist 🌟", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/18button.jpg` },
+        { text: "Curvy Hips 🍑", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/19button.jpg` },
+        { text: "Bare Back 🌹", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/20button.jpg` },
+        { text: "Beautiful Hands 💅", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/m.jpeg` },
+        { text: "Formfitted Allover 💃", value: 7, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/n.jpeg` },
       ],
     },
     {
-      question: "Which classic novel do you wish you could live in?",
+      question: "Pick Your Bridal Inspiration?",
       options: [
-        { text: "The Secret Garden by Frances Hodgson Burnett", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/21button.jpg` },
-        { text: "Pride and Prejudice by Jane Austen", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/22button.jpg` },
-        { text: "The Great Gatsby by F. Scott Fitzgerald", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/23button.jpg` },
-        { text: "Brideshead Revisited by Evelyn Waugh", value: 7, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/24button.jpg` },
+        { text: "Elle Fanning as Aurora 🌸", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/elle.jpeg` },
+        { text: "Phantom Of The Opera 🎭", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/christine.jpeg` },
+        { text: "Rogers & Hammerstein’s Cinderella 👗", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/cinderella.jpeg` },
+        { text: "Bella Swan 🌲", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/bella.jpeg` },
+        { text: "Kate Middleton 👑", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/kate.jpeg` },
+        { text: "Lana Del Rey 🎶", value: 6, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/lana.jpeg` },
       ],
     },
     {
       question: "Dream Wedding Gift",
       options: [
-        { text: "Leather Scrapbook", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/25button.jpg` },
-        { text: "Pearl Necklace", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/26button.jpg` },
-        { text: "Glamorous Mirror", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/27button.jpg` },
-        { text: "Vintage Silver Tea Set", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/28button.jpg` },
+        { text: "Leather Scrapbook 📚", value: 1, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/25button.jpg` },
+        { text: "Pressed Florals 🌼", value: 2, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/pressedflowers.jpeg` },
+        { text: "Pearl Necklace 📿", value: 3, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/26button.jpg` },
+        { text: "Chanel Bag 👜", value: 4, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/chanelbag.jpeg` },
+        { text: "Luxurious Stationary ✉️", value: 5, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/stationary.jpeg` },
+        { text: "Vintage Silver Tea Set 🫖", value: 7, backgroundImage: `${process.env.PUBLIC_URL}/Images/Quiz/Quiz3/28button.jpg` },
       ],
     },
   ];
+
+  useEffect(() => {
+    const updateButtonHeight = () => {
+      const vh = window.innerHeight;
+      const headerHeight = document.querySelector('.question-section').offsetHeight;
+      const remainingHeight = vh - headerHeight - 40; // Adjusting for padding and margins
+      const buttonHeight = Math.floor(remainingHeight / 3) - 10; // Adjusting for gap and 3 rows
+      setButtonHeight(buttonHeight);
+    };
+
+    updateButtonHeight();
+    window.addEventListener('resize', updateButtonHeight);
+    return () => window.removeEventListener('resize', updateButtonHeight);
+  }, [currentQuestionIndex]);
 
   const handleAnswerClick = (value) => {
     setAnswers([...answers, value]);
@@ -94,9 +123,9 @@ const Quiz3 = () => {
     if (total <= 10) {
       result = 'Fairytale Wedding - Bacarra';
     } else if (total <= 18) {
-      result = 'Fairytale Wedding - Elizabeth';
+      result = 'Fairytale Wedding 2 - Elizabeth';
     } else if (total <= 26) {
-      result = 'Vintage Glamour - Percy';
+      result = 'Simple & Chic - Percy';
     } else if (total <= 34) {
       result = 'Old Money - Ora';
     } else if (total <= 42) {
@@ -104,25 +133,24 @@ const Quiz3 = () => {
     } else if (total <= 48) {
       result = 'Vintage Glamour - Porsha';
     } else {
-      result = 'Old Money - Zara';
+      result = 'Bridgerton Vibes - Zara';
     }
 
     // Redirect to the result page based on the calculated result
     switch (result) {
       case 'Fairytale Wedding - Bacarra': navigate('/bacarra'); break;
-      case 'Fairytale Wedding - Elizabeth': navigate('/elizabeth'); break;
-      case 'Vintage Glamour - Percy': navigate('/percy'); break;
+      case 'Fairytale Wedding 2 - Elizabeth': navigate('/elizabeth'); break;
+      case 'Simple & Chic - Percy': navigate('/percy'); break;
       case 'Old Money - Ora': navigate('/ora'); break;
       case 'Royal Wedding - Abril': navigate('/abril'); break;
       case 'Vintage Glamour - Porsha': navigate('/porsha'); break;
-      case 'Old Money - Zara': navigate('/zara'); break;
+      case 'Bridgerton Vibes - Zara': navigate('/zara'); break;
       default: navigate('/');
     }
   };
 
   return (
     <div className="quiz-container">
-      <div>
       <Helmet>
         <title>The Ultimate 2025 Wedding Dress Guide</title>
         <meta name="description" content="Find your perfect wedding dress style with our fun and interactive quiz! Discover which wedding dress matches your dream wedding location, style, and more." />
@@ -151,9 +179,7 @@ const Quiz3 = () => {
       {!showResults ? (
         <>
           <div className="question-section">
-            <div className="question-count">
-              <span>QUESTION {currentQuestionIndex + 1}</span>/{quizQuestions.length}
-            </div>
+           
             <div className="question-text">{quizQuestions[currentQuestionIndex].question}</div>
           </div>
           <div className="answer-section">
@@ -162,6 +188,7 @@ const Quiz3 = () => {
                 key={index}
                 className="answer-button"
                 style={{
+                  height: `${buttonHeight}px`,
                   backgroundImage: `url(${option.backgroundImage})`,
                   backgroundSize: 'cover',
                   backgroundPosition: 'center',
@@ -179,12 +206,9 @@ const Quiz3 = () => {
           <button onClick={() => navigate('/')}>Go Home</button>
         </div>
       )}
+      <AdSenseAd />
     </div>
-  
-      <AdSenseAd/>
-      </div>
   );
 };
 
 export default Quiz3;
-
