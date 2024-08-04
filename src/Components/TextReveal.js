448baf223740b978d878852ec53255a25484d5ec
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-08-03 20:17:36 -0400

    43

--- src/Components/TextReveal.js
+++ src/Components/TextReveal.js
@@ -4,7 +4,7 @@ import styles from './TextReveal.module.css'; // Import the CSS module
 const TextReveal = (props) => {
   return (
     <div className={styles.hero} style={props.style}>
-      <h1 className={styles.textReveal} style={{ fontFamily: props.fontFamily }}>
+      <h1 className={styles.textReveal}>
         {props.text}
       </h1>
     </div>
