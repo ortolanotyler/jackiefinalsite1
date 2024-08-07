016542edda921598fdba6123a0fd87105180cbac
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-08-07 17:24:25 -0400

    43

--- src/Home/QuoteBanner3.js
+++ src/Home/QuoteBanner3.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import styles from './QuoteBanner.module.css'; // Importing the CSS module
+import './QuoteBanner.module.css'; // Importing the CSS file for the quote banner
+import PageLines from '../Components/PageBreakUpLines';
+
+
+
+
 
 const QuoteBanner3 = () => {
   return (
-    <div className={styles.quoteBanner}>
-  
-      <blockquote className={styles.blockquote}>
-        "Switch up your style with confidence”
-      </blockquote>
-      <cite className={styles.cite}>— Jackie Wyers</cite>
-    
+    <div 
+    className="quote-banner">
+      <PageLines/>
+      <blockquote>
+      "Switch up your style with confidence”      </blockquote>
+      <cite>— Jackie Wyers</cite>
+      <PageLines/>
     </div>
   );
 };
