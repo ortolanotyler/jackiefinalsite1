bc7a4ae0ebcb9e7545a966926c857332e3b69864
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-08-07 16:44:41 -0400

    43

--- src/Home/ImageGridTutorials.js
+++ src/Home/ImageGridTutorials.js
@@ -3,14 +3,16 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
 // Import your images
-const image1 = `${process.env.PUBLIC_URL}/Images/Home/HomePage1.webp`;
-const image2 = `${process.env.PUBLIC_URL}/Images/Home/HomePage2.webp`;
-const image3 = `${process.env.PUBLIC_URL}/Images/Home/HomePage3.webp`;
-const image4 = `${process.env.PUBLIC_URL}/Images/Home/HomePage4.webp`;
-const image5 = `${process.env.PUBLIC_URL}/Images/Home/HomePage5.webp`;
-const image6 = `${process.env.PUBLIC_URL}/Images/Home/HomePage6.webp`;
-const image7 = `${process.env.PUBLIC_URL}/Images/Home/HomePage7.webp`;
-const image8 = `${process.env.PUBLIC_URL}/Images/Home/HomePage8.webp`;
+const image1 = `${process.env.PUBLIC_URL}/Images/Home/HomePage1.jpg`;
+const image2 = `${process.env.PUBLIC_URL}/Images/Home/HomePage2.jpg`;
+const image3 = `${process.env.PUBLIC_URL}/Images/Home/HomePage3.jpg`;
+const image4 = `${process.env.PUBLIC_URL}/Images/Home/HomePage4.jpg`;
+const image5 = `${process.env.PUBLIC_URL}/Images/Home/HomePage5.jpg`;
+const image6 = `${process.env.PUBLIC_URL}/Images/Home/HomePage6.jpg`;
+const image7 = `${process.env.PUBLIC_URL}/Images/Home/HomePage7.jpg`;
+const image8 = `${process.env.PUBLIC_URL}/Images/Home/HomePage8.jpg`;
+
+
 
 const itemData = [
   {
@@ -37,30 +39,34 @@ const itemData = [
     img: image6,
     title:'Jackie Wyers Barbie Margot',
   },
-  {
-    img: image7,
-    title: 'Jackie Wyers in San Diego',
-  },
-  {
-    img: image8,
-    title:"Jackie Wyers Penhaligon's Campaign",
-  },
+    {
+        img: image7,
+        title: 'Jackie Wyers in San Diego',
+    },
+    {
+        img: image8,
+        title:"Jackie Wyers Penhaligon's Campaign",
+    },
+
+  // ... repeat for each image ...
+
 ];
 
 export default function QuiltedImageList1() {
   return (
     <ImageList
-      sx={{ width: '100%', height: '55vh', boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.9)' }}
+      sx={{ width: '100%', height: '55vh',  boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.9}' }}
       variant="quilted"
       cols={4}
+   
+      
     >
       {itemData.map((item, index) => (
         <ImageListItem key={index} cols={item.cols || 1} rows={item.rows || 1}>
           <img
             src={item.img}
             alt={item.title}
-            style={{ width: '100%', height: 'auto' }}
-            loading="eager" // Ensure all images load eagerly
+            loading="lazy"
           />
         </ImageListItem>
       ))}
