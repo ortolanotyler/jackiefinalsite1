79c8df6403d8db4816ca6faed01d3a8a02053787
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-16 21:40:45 -0400

    43

--- src/Components/NewsletterSlider.js
+++ src/Components/NewsletterSlider.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import './NewsletterSlider.module.css'; // Ensure this file exists
 
 const images = [
-  `${process.env.PUBLIC_URL}/Images/Home/1.png`,
-  `${process.env.PUBLIC_URL}/Images/Home/2.png`,
-  `${process.env.PUBLIC_URL}/Images/Home/3.png`,
-  `${process.env.PUBLIC_URL}/Images/Home/4.png`,
-
+  `${process.env.PUBLIC_URL}/Images/1.png`,
+  `${process.env.PUBLIC_URL}/Images/2.png`,
+  `${process.env.PUBLIC_URL}/Images/3.png`,
+  `${process.env.PUBLIC_URL}/Images/4.png`,
 ];
 
 const NewsletterSlider = () => {
@@ -23,49 +22,66 @@ const NewsletterSlider = () => {
   };
 
   return (
-    <Box 
-      className="sliderContainer" 
-      sx={{ 
-        position: 'relative', 
-        width: '100%', 
-        maxWidth: '370px', 
-        maxHeight: '600px', 
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center',
-        margin: '0 auto', // Centering the Box within its container
-      }}
-    >
-      <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex + 1}`}
-        className="sliderImage"
-        style={{
-          width: '100%',
-          maxWidth: '500px',
-          maxHeight: '560px',
-          height: 'auto',
-          borderRadius: '5px',
-          objectFit: 'cover',
+    <React.Fragment>
+           {/* Typography below the slider */}
+           <Typography 
+        variant="h2" 
+        align="center" 
+        sx={{ 
+          
+     
+          fontFamily: 'GFS Didot, serif', 
+          color: '#000000', 
         }}
-      />
+      >
+        Sign Up for the weekly newsletter!
+      </Typography>
+      <Box 
+        className="sliderContainer" 
+        sx={{ 
+          position: 'relative', 
+        
+          width: '100%', 
+          maxWidth: '900px', 
+          maxHeight: '100%', 
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center',
+          margin: '1rem auto', // Centering the Box within its container
+          
+        }}
+      >
+        <img
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          className="sliderImage"
+          style={{
+            width: '100%',
+            maxWidth: '650px',
+            height: 'auto',
+            borderRadius: '0px',
+            objectFit: 'cover',
+          }}
+        />
 
-      <IconButton
-        className="prevButton"
-        sx={{ position: 'absolute', top: '50%', left: '10px', transform: 'translateY(-50%)', color: 'black',  }}
-        onClick={handlePrevClick}
-      >
-        <ArrowBack />
-      </IconButton>
-      <IconButton
-        className="nextButton"
-        sx={{ position: 'absolute', top: '50%', right: '10px', transform: 'translateY(-50%)', color: 'black' }}
-        onClick={handleNextClick}
-      >
-        <ArrowForward />
-      </IconButton>
-   
-    </Box>
+        <IconButton
+          className="prevButton"
+          sx={{ position: 'absolute', top: '50%', left: '10px', transform: 'translateY(-50%)', color: 'black' }}
+          onClick={handlePrevClick}
+        >
+          <ArrowBack />
+        </IconButton>
+        <IconButton
+          className="nextButton"
+          sx={{ position: 'absolute', top: '50%', right: '10px', transform: 'translateY(-50%)', color: 'black' }}
+          onClick={handleNextClick}
+        >
+          <ArrowForward />
+        </IconButton>
+      </Box>
+
+ 
+    </React.Fragment>
   );
 };
 
