16b4f9ff04b515cc84c96f7a1217e943a12c564f
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-17 20:30:27 -0400

    43

--- src/Articles/Tutorials/VintageVibes/Yardley1965.js
+++ src/Articles/Tutorials/VintageVibes/Yardley1965.js
@@ -60,6 +60,8 @@ const paragraphStyle = {
   marginRight: 'auto',
 };
 
+
+
   const blogContent = (
     <div className="container">
 <Helmet>
@@ -150,147 +152,135 @@ style = {{
 
   <AdSenseAd/>
 </div>
-      <h2 style={headingStyle}>The Inspiration: Oh! De London by Yardley</h2>
+<h2 style={headingStyle}>The Inspiration: Oh! De London by Yardley</h2>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <a href="https://www.instagram.com/reel/CNyCIQkBUOK/?hl=en" target="_blank" rel="noopener noreferrer">
-          <img src={image1} alt="Jean Shrimpton Look" style={{ width: '100%', maxWidth: '650px' }} />
-        </a>
-      </div>
-      <p style={{ textAlign: 'center', fontSize: '1rem', fontFamily: 'Arapey, sans-serif', margin: '0em', maxWidth: '100%', fontStyle: 'italic' }}>
-        Original Makeup & Hair Design by Fae Hammond from the 2005 film 'Pride and Prejudice,' recreation by Jackie Wyers.
-      </p>
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  <a href="https://www.instagram.com/reel/CNyCIQkBUOK/?hl=en" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: '#000' }}>
+    <img src={image1} alt="Jean Shrimpton Look" style={{ width: '100%', maxWidth: '650px' }} />
+  </a>
+</div>
 
-      <p style={paragraphStyle}>
-        Let's delve into the inspiration which begins with an homage to the iconic Yardley London Oh! De London Cologne advertisement from 1965. The Yardley ads of the '60s are renowned for their vivid colours, striking lines, and whimsical charm, embodying the essence of the influential pop art movement of the era. These ads captivated audiences with their trendy MOD style.
-      </p>
+<p style={{ textAlign: 'center', fontSize: '1rem', fontFamily: 'Arapey, sans-serif', margin: '0em', maxWidth: '100%', fontStyle: 'italic' }}>
+  Original Makeup & Hair Design by Fae Hammond from the 2005 film 'Pride and Prejudice,' recreation by Jackie Wyers.
+</p>
 
-      <p style={paragraphStyle}>
-        Luckily, I was able to find the fragrance, and although it has aged, the scent retains a powdery whisper from the past. I surrounded myself with its aroma to immerse myself fully in the spirit of the era, making it the ideal muse for this makeup transformation.
-      </p>
+<p style={paragraphStyle}>
+  Let's delve into the inspiration which begins with an homage to the iconic <a href="https://www.yardleylondon.co.uk" target="_blank" rel="noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Yardley London Oh! De London Cologne</a> advertisement from 1965. The Yardley ads of the '60s are renowned for their vivid colours, striking lines, and whimsical charm, embodying the essence of the influential pop art movement of the era. These ads captivated audiences with their trendy MOD style.
+</p>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <a href="https://www.pinterest.ca/jackiewyers/vintage-vibes-makeup-hair-inspiration/" target="_blank" rel="noopener noreferrer">
-          <img src={face} alt="Jean Shrimpton Face Chart" style={{ width: '100%', maxWidth: '650px' }} />
-        </a>
-      </div>
+<p style={paragraphStyle}>
+  Luckily, I was able to find the fragrance, and although it has aged, the scent retains a powdery whisper from the past. I surrounded myself with its aroma to immerse myself fully in the spirit of the era, making it the ideal muse for this makeup transformation.
+</p>
 
-      <h2 style={headingStyle}>Youthful Base & Matte Finish Foundation</h2>
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  <a href="https://www.pinterest.ca/jackiewyers/vintage-vibes-makeup-hair-inspiration/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: '#000' }}>
+    <img src={face} alt="Jean Shrimpton Face Chart" style={{ width: '100%', maxWidth: '650px' }} />
+  </a>
+</div>
 
-      <p style={paragraphStyle}>
-        I began with the application of faux bangs and extensions, setting the stage for a transformative look. For the base makeup, I chose a radiant primer to minimize pores and prepare my skin for the foundation—a natural velvet matte finish foundation that evokes the era's flawless complexion, paired with a yellow-based concealer to brighten and balance the skin tone.
-      </p>
-      <div
-style = {{
-  margin : '20px'
-}}
->
+<h2 style={headingStyle}>Youthful Base & Matte Finish Foundation</h2>
 
+<p style={paragraphStyle}>
+  I began with the application of faux bangs and extensions, setting the stage for a transformative look. For the base makeup, I chose a radiant primer to minimize pores and prepare my skin for the foundation—a natural velvet matte finish foundation that evokes the era's flawless complexion, paired with a yellow-based concealer to brighten and balance the skin tone.
+</p>
+
+<div style={{ margin: '20px' }}>
   <AdSenseAd/>
 </div>
-      <h2 style={headingStyle}>Sculpting the Features</h2>
 
-      <p style={paragraphStyle}>
-        To achieve the iconic '60s facial contours, I added a bit of nose contouring magic, connecting my brows to my nose with two little lines and defining the bridge to mimic the supermodel. Skin is further complemented by a soft, cream blush in a coral shade to the cheeks, bringing life and colour to the look.
-      </p>
+<h2 style={headingStyle}>Sculpting the Features</h2>
 
-      <ResponsiveIframe src="https://shopmy.us/collections/public/524886?noHeader=true" title="Shop My Victoria's Secret Favorites" />
+<p style={paragraphStyle}>
+  To achieve the iconic '60s facial contours, I added a bit of nose contouring magic, connecting my brows to my nose with two little lines and defining the bridge to mimic the supermodel. Skin is further complemented by a soft, cream blush in a coral shade to the cheeks, bringing life and colour to the look.
+</p>
 
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+<ResponsiveIframe src="https://shopmy.us/collections/public/524886?noHeader=true" title="Shop My Victoria's Secret Favorites" />
 
-      <h2 style={headingStyle}>Eyes That Speak Volumes</h2>
+<p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
+  CONTAINS: AFFILIATE LINKS
+</p>
 
-      <p style={paragraphStyle}>
-        The eyes are the centrepiece of this '60s inspired look. I opted for an easy, to achieve method using a creamy eyeliner to sketch a higher crease in a half-moon shape. A deeper brown eyeshadow also works perfect for this step. Then blend with a peach shadow in the inner crease for a soft yet defined eye contour. There are warm pops of peach and coral throughout this style, so this creates a harmonious look.
-      </p>
+<h2 style={headingStyle}>Eyes That Speak Volumes</h2>
 
-      <p style={paragraphStyle}>
-        A pop of silvery white on the lid, complemented by dramatic Twiggy-inspired liner and lashes, brought the look together, capturing the essence of the decade.
-      </p>
+<p style={paragraphStyle}>
+  The eyes are the centrepiece of this '60s inspired look. I opted for an easy, to achieve method using a creamy eyeliner to sketch a higher crease in a half-moon shape. A deeper brown eyeshadow also works perfect for this step. Then blend with a peach shadow in the inner crease for a soft yet defined eye contour. There are warm pops of peach and coral throughout this style, so this creates a harmonious look.
+</p>
 
-      <ResponsiveIframe src="https://shopmy.us/collections/public/524889?noHeader=true" title="Shop My Victoria's Secret Favorites" />
+<p style={paragraphStyle}>
+  A pop of silvery white on the lid, complemented by dramatic Twiggy-inspired liner and lashes, brought the look together, capturing the essence of the decade.
+</p>
 
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '-0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
-      <div
-style = {{
-  margin : '20px'
-}}
->
+<ResponsiveIframe src="https://shopmy.us/collections/public/524889?noHeader=true" title="Shop My Victoria's Secret Favorites" />
 
+<p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '-0.5em 0', maxWidth: '100%' }}>
+  CONTAINS: AFFILIATE LINKS
+</p>
+
+<div style={{ margin: '20px' }}>
   <AdSenseAd/>
 </div>
-      <h2 style={headingStyle}>Lips and Final Touches</h2>
 
-      <p style={paragraphStyle}>
-        For the lips, I chose a peachy gloss to keep things light and springy, outlined with a lip liner to define the shape using Charlotte Tilbury’s Lip Cheat Lip Liner in Supersize Me.
-      </p>
+<h2 style={headingStyle}>Lips and Final Touches</h2>
 
-      <ResponsiveIframe src="https://shopmy.us/collections/public/524891?noHeader=true" title="Shop My Victoria's Secret Favorites" />
+<p style={paragraphStyle}>
+  For the lips, I chose a peachy gloss to keep things light and springy, outlined with a lip liner to define the shape using <a href="https://www.charlottetilbury.com/us/product/lip-cheat-supersize-me" target="_blank" rel="noopener noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Charlotte Tilbury’s Lip Cheat Lip Liner in Supersize Me</a>.
+</p>
 
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+<ResponsiveIframe src="https://shopmy.us/collections/public/524891?noHeader=true" title="Shop My Victoria's Secret Favorites" />
 
-      <p style={paragraphStyle}>
-        The finishing touch was a highlighter to accentuate the high points of the face, adding a modern twist to the vintage look. Charlotte Tilbury’s Beauty Light Wand in Peachgasm was perfect for this!
-      </p>
+<p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
+  CONTAINS: AFFILIATE LINKS
+</p>
 
-      <ResponsiveIframe src="https://shopmy.us/collections/public/524892?noHeader=true" title="Shop My Victoria's Secret Favorites" />
+<p style={paragraphStyle}>
+  The finishing touch was a highlighter to accentuate the high points of the face, adding a modern twist to the vintage look. <a href="https://www.charlottetilbury.com/us/product/beauty-light-wand-peachgasm" target="_blank" rel="noopener noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Charlotte Tilbury’s Beauty Light Wand in Peachgasm</a> was perfect for this!
+</p>
 
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
-      <div
-style = {{
-  margin : '20px'
-}}
->
+<ResponsiveIframe src="https://shopmy.us/collections/public/524892?noHeader=true" title="Shop My Victoria's Secret Favorites" />
 
+<p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5rem' }}>
+  CONTAINS: AFFILIATE LINKS
+</p>
+
+<div style={{ margin: '20px' }}>
   <AdSenseAd/>
 </div>
-      <h2 style={headingStyle}>The Hair: An Epic Updo Challenge</h2>
 
-      <p style={paragraphStyle}>
-        No '60s look would be complete without an iconic hairstyle. I challenged myself to creating an ambitious updo, utilizing extensions to achieve the volume and structure characteristic of the era. By pinning loops to craft a voluminous bun, I emulated the vintage style, adorning it with flowers and bows that were true to the period.
-      </p>
+<h2 style={headingStyle}>The Hair: An Epic Updo Challenge</h2>
 
-      <p style={paragraphStyle}>
-        The flower accessories, to my delight, resemble paper! I sourced some paper flowers and handcrafted a few bows, embellishing them with pearls to render the floral portrait as authentic as possible. Incidentally, I stumbled upon a book on crafting paper flowers, which I've linked above—could this be a new hobby for spring?
-      </p>
+<p style={paragraphStyle}>
+  No '60s look would be complete without an iconic hairstyle. I challenged myself to creating an ambitious updo, utilizing extensions to achieve the volume and structure characteristic of the era. By pinning loops to craft a voluminous bun, I emulated the vintage style, adorning it with flowers and bows that were true to the period.
+</p>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img src={ad} alt="Jean Shrimpton Ad" style={{ width: '100%', maxWidth: '650px' }} />
-      </div>
+<p style={paragraphStyle}>
+  The flower accessories, to my delight, resemble paper! I sourced some paper flowers and handcrafted a few bows, embellishing them with pearls to render the floral portrait as authentic as possible. Incidentally, I stumbled upon a book on crafting paper flowers, which I've linked above—could this be a new hobby for spring?
+</p>
 
-      <h2 style={headingStyle}>A Vintage Collection to Adore</h2>
+<div style={{ display: 'flex', justifyContent: 'center' }}>
+  <img src={ad} alt="Jean Shrimpton Ad" style={{ width: '100%', maxWidth: '650px' }} />
+</div>
 
-      <p style={paragraphStyle}>
-        As a lover of all things vintage, I couldn't resist sharing a glimpse into my Yardley vintage collection that I hope to continue to collect. The packaging alone transports us back in time and I’m BEGGING Yardley to do a throwback collection. Their soaps you can still buy today are lovely though! Creating this look was a delightful challenge, blending my love for vintage aesthetics with the thrill of using thrifting '60s makeup products. I hope this tutorial inspires you to explore the beauty of past decades and perhaps go on your own vintage beauty adventure.
-      </p>
+<h2 style={headingStyle}>A Vintage Collection to Adore</h2>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img src={image5} alt="Vintage Collection" style={{ width: '100%', maxWidth: '650px' }} />
-      </div>
-      <div
-style = {{
-  margin : '20px'
-}}
->
+<p style={paragraphStyle}>
+  As a lover of all things vintage, I couldn't resist sharing a glimpse into my <a href="https://www.yardleylondon.co.uk" target="_blank" rel="noreferrer" style={{ color: '#000', textDecoration: 'underline' }}>Yardley vintage collection</a> that I hope to continue to collect. The packaging alone transports us back in time and I’m BEGGING Yardley to do a throwback collection. Their soaps you can still buy today are lovely though! Creating this look was a delightful challenge, blending my love for vintage aesthetics with the thrill of using thrifting '60s makeup products. I hope this tutorial inspires you to explore the beauty of past decades and perhaps go on your own vintage beauty adventure.
+</p>
 
+<div style={{ display: 'flex', justifyContent: 'center' }}>
+  <img src={image5} alt="Vintage Collection" style={{ width: '100%', maxWidth: '650px' }} />
+</div>
+
+<div style={{ margin: '20px' }}>
   <AdSenseAd/>
 </div>
-      <p style={paragraphStyle}>
-        If you have any vintage icons you'd like me to cover in future posts, don't hesitate to leave a comment below. And remember, the beauty of vintage is not just in the look—it's in the stories we tell and the history we keep alive. If you enjoyed this look, you may love Sharon Tate's unique blend of bohemian and mod aesthetics. I’ve also covered her coquette bridal style, <a href="/sharontatebridal" style={{ color: 'black' }} target="_blank" rel="noopener noreferrer">here</a>.
-      </p>
 
-      <p style={paragraphStyle}>
-        Thank you for reading and if you want to be featured on my site, try out this look and please tag me as I’d love to see it and share your rendition! I love to see you switch up your style with confidence.
-      </p>
+<p style={paragraphStyle}>
+  If you have any vintage icons you'd like me to cover in future posts, don't hesitate to leave a comment below. And remember, the beauty of vintage is not just in the look—it's in the stories we tell and the history we keep alive. If you enjoyed this look, you may love <a href="/sharontatebridal" style={{ color: '#000', textDecoration: 'underline' }} target="_blank" rel="noopener noreferrer">Sharon Tate's unique blend of bohemian and mod aesthetics</a>. I’ve also covered her coquette bridal style, <a href="/sharontatebridal" style={{ color: '#000', textDecoration: 'underline' }} target="_blank" rel="noopener noreferrer">here</a>.
+</p>
 
+<p style={paragraphStyle}>
+  Thank you for reading and if you want to be featured on my site, try out this look and please tag me as I’d love to see it and share your rendition! I love to see you switch up your style with confidence.
+</p>
       <div style={{ textAlign: 'center', margin: '2rem 0' }}>
         <a href="/sharontatebridal" style={{ fontFamily: 'Arapey', fontSize: '1.25rem', color: 'black', textDecoration: 'underline', textAlign: 'center' }}>
           <img src={image6} alt="Margot Robbie - Barbie Movie" style={{ width: '100%', maxWidth: '500px', margin: '0 auto', display: 'block' }} />
