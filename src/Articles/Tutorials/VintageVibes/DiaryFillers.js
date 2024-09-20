b3ca078195a1e020aed906ee8646960f11fc95e9
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-20 17:32:34 -0400

    43

--- src/Articles/Tutorials/VintageVibes/DiaryFillers.js
+++ src/Articles/Tutorials/VintageVibes/DiaryFillers.js
@@ -6,87 +6,140 @@ import { Helmet } from 'react-helmet';
 import AdSenseAd from '../../../Advertising/Ads';
 import { initGA, logPageView } from '../../../analytics';
 import DropCap1 from '../../Travel/DropCap1';
+import ArticleTitle from '../../../Components/ArticleTitle';
+import NextArticle from '../../../Components/NextArticleComponent';
+import HorizontalLine from '../../../Components/HorizontalLine';
 
 const title = `${process.env.PUBLIC_URL}/Images/Articles/DiaryPost1/DiaryPostTitle.png`;
-const image2 = `${process.env.PUBLIC_URL}/Images/Articles/DiaryPost1/Diary2.png`;
+const bridesmaid = `${process.env.PUBLIC_URL}/Images/Articles/Bridesmaids/BridesmaidBoxThumb!.jpg`;
 const signature = `${process.env.PUBLIC_URL}/Images/Articles/Signature.png`;
+const image2 = `${process.env.PUBLIC_URL}/Images/Articles/DiaryPost1/Diary2.png`;
+
+
 
 const Card = ({ blogContent }) => (
-<div style = {{
- padding: '10px 20px',
-  margin: '0 auto',
-}}>
-
-    <div className="card">
-      <div className="blog-content">{blogContent}</div>
-    </div>
-    </div>
-);
-
-const DiaryPost1Fillers = () => {
-  const websiteId = '10910';
-  const blogRef = useRef(null);
-  
-  useEffect(() => {
-    initGA();
-    logPageView('/diaryfillers');
-}, []);
-
  
-const headingStyle = {
-  textAlign: 'center',
-  fontSize: '1.5rem',
-  fontWeight: 'normal',
-  fontFamily: 'Arapey, serif',
-  color: '#333',
-  margin: '1.5rem'
-};
-
-const paragraphStyle = {
-  fontSize: '20px',
-  fontFamily: 'GFS Didot, serif',
-  margin: '1rem 1.5rem',
-  marginTop: '1rem',
-  marginBottom: '1rem',
-  maxWidth: '500px',
-  lineHeight: '1.5',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-};
+  <div style = {{
+   padding: '10px 20px',
+    margin: '0 auto',
+  }}>
+  
+      <div className="card">
+        <div className="blog-content">{blogContent}</div>
+      </div>
+      </div>
+  
+    
+  );
+  
+  const DiaryPost1Fillers = () => {
+  
+    useEffect(() => {
+      initGA();
+      logPageView('/diaryfillers');
+    }, []);
+  
+  
+    const websiteId = '10910';
+    const blogRef = useRef(null);
+  
+   
+    const headingStyle = {
+      textAlign: 'center',
+      fontSize: '2.5rem',
+      fontWeight: 'normal',
+      fontFamily: 'GFS Didot, serif',
+      color: '#000000',
+      margin: '30px auto'
+  };
+  
+  
+  const paragraphStyle = {
+    fontSize: '22px',
+    color: '#000000',
+  
+    fontFamily: 'GFS Didot, serif',
+    margin: '2rem auto',
+  
+    maxWidth: '500px', // Consistent with the other paragraphs
+    lineHeight: '1.6',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  };
+  
+  const linkStyle = {
+    textDecoration: 'none', // Removes underline from links
+    fontFamily: 'GFS Didot, serif',
+    color: '#000000', // Inherits the color of the surrounding text
+  };
+  
+  const gridContainerStyle = {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+      gap: '10px',
+      margin: '1rem auto'
+  };
+  
   const imageStyle = {
     display: 'block',
-    margin: '0 auto',
+    margin: '1rem auto',
+    borderRadius: '0px',
+    maxWidth: '500px',
+    height: 'auto'
+  };
+  
+  const pinterestGridStyle = {
+    display: 'grid',
+    gap: '1rem',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(236px, 1fr))', // Adjusts columns based on screen size
+    justifyItems: 'center',
+    margin: '1rem 0',
+  };
+  
+  const iframeStyle = {
+    width: '100%',
+    maxWidth: '236px',
+    height: '520px', // Maintains original height
+    border: 'none',
+    scrolling: 'no',
+  };
+  
+  const captionStyle = {
+    textAlign: 'center',
+    fontSize: '1.1rem',
+    fontFamily: 'Playfair Display, serif',
+    margin: '1rem auto',
     maxWidth: '100%',
-    maxHeight: '900px',
-    objectFit: 'cover',
+    fontStyle: 'italic',
   };
 
   const blogContent = (
     <div className="container">
-    <Helmet>
-  <title>Diary Post #1: My Journey with Injectable Fillers - Jackie Wyers</title>
+ <Helmet>
+  <title>Beyond the Surface: My Experience with Injectable Fillers and Self-Acceptance</title>
   <link rel="canonical" href="https://jackiewyers.beauty/diaryfillers" />
 
   <meta name="description" content="Jackie Wyers shares her personal experience with getting injectable fillers at a young age, reflecting on insecurities, decisions, and growth." />
-  <meta name="keywords" content="Diary, Injectable Fillers, Jackie Wyers, Personal Experience, Beauty Blog, Cosmetic Enhancements, Self-Esteem, Beauty Standards, Personal Growth, Beauty Journey" />
+  <meta name="keywords" content="Diary, Injectable Fillers, Jackie Wyers, Personal Experience, Beauty Blog, Cosmetic Enhancements, Self-Esteem, Beauty Standards, Personal Growth, Beauty Journey, non-surgical rhinoplasty, YouTube influencer, beauty transformation stories, confidence journey, beauty standards impact, cosmetic procedures reflection" />
+
   <meta property="og:title" content="Diary Post: My Journey with Injectable Fillers - Jackie Wyers" />
   <meta property="og:description" content="Jackie Wyers shares her personal experience with getting injectable fillers at a young age, reflecting on insecurities, decisions, and growth." />
-  <meta property="og:image" content={title} />
+  <meta property="og:image" content="https://jackiewyers.beauty/Images/Articles/DiaryPost1/DIARYSQUARE.jpg" />
   <meta property="og:url" content="https://jackiewyers.beauty/diaryfillers" />
-  <meta name="twitter:card" content="summary_large_image" />
+
   <meta name="twitter:title" content="Diary Post: My Journey with Injectable Fillers - Jackie Wyers" />
   <meta name="twitter:description" content="Jackie Wyers shares her personal experience with getting injectable fillers at a young age, reflecting on insecurities, decisions, and growth." />
-  <meta name="twitter:image" content={title} />
+  <meta name="twitter:image" content="https://jackiewyers.beauty/Images/Articles/DiaryPost1/DIARYSQUARE.jpg" />
+  <meta name="twitter:card" content="summary_large_image" />
+
+  {/* Google Analytics Script */}
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-RT6GR7JXYG"></script>
-  <script>
-      {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-RT6GR7JXYG');
-      `}
-  </script>
+  <script src={`${process.env.PUBLIC_URL}/analytics.js`} async></script>
+
+  {/* Google Ads Script */}
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4660168246825318" crossorigin="anonymous"></script>
+
+  {/* JSON-LD Structured Data */}
   <script type="application/ld+json">
     {`
       {
@@ -94,7 +147,7 @@ const paragraphStyle = {
         "@type": "BlogPosting",
         "headline": "Diary Post #1: My Journey with Injectable Fillers - Jackie Wyers",
         "description": "Jackie Wyers shares her personal experience with getting injectable fillers at a young age, reflecting on insecurities, decisions, and growth.",
-        "image": "${title}",
+        "image": "https://jackiewyers.beauty/Images/Articles/DiaryPost1/DIARYSQUARE.jpg",
         "author": {
           "@type": "Person",
           "name": "Jackie Wyers"
@@ -107,8 +160,8 @@ const paragraphStyle = {
             "url": "https://jackiewyers.beauty/logo.png"
           }
         },
-        "datePublished": "2024-07-27",
-        "dateModified": "2024-07-27",
+        "datePublished": "2024-05-03",
+        "dateModified": "2024-09-20",
         "mainEntityOfPage": {
           "@type": "WebPage",
           "@id": "https://jackiewyers.beauty/diaryfillers"
@@ -120,46 +173,55 @@ const paragraphStyle = {
 
 
     
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '15vh' }}>
-        <a href="/diary" style={{ fontFamily: 'Arapey', fontSize: '20px', color: 'black', textDecoration: 'none' }}>
+<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+        <a href="/diary" style={{ fontFamily: 'GFS Didot, serif', fontSize: '1rem', color: '#000000', textDecoration: 'none' }}>
           DIARY
         </a>
       </div>
-      <img src={title} alt="Diary Post Title" style={{ width: '100%' }} />
 
+      <ArticleTitle
+  mainTitle="Beyond the Surface: My Experience with Injectable Fillers and Self-Acceptance"
+  subTitle="From external enhancements to inner peace"
+  author="Jackie Wyers"
+  publishDate="May 3rd, 2024"
+/>
+
+
+<HorizontalLine/>
+<br/>
 
 
       <DropCap1 text="Welcome back to my blog for my first-ever diary entry, where I dive deep into my personal experience with getting injectable fillers at a young age. In today's social media-driven world, it's increasingly common to see teens and young women experimenting with cosmetic enhancements. Platforms like Instagram flood us with transformation reels and personal stories from influencers, making such procedures seem both normal and desirable. While this transparency can be enlightening, it also carries significant influence, shaping perceptions and expectations about beauty." />
-      <div
-style = {{
-  margin : '20px'
-}}
->
-
-  <AdSenseAd/>
-</div>
+    
       <h2 style={headingStyle}>Early Insecurities and the Drive for Control</h2>
 
       <p style={paragraphStyle}>
         Starting at 12, I struggled with my self-image, especially during the awkward stages of puberty. Unlike some friends, who were excited about their teen years, I felt an overwhelming shame about the changes in my body.
       </p>
+      
 
       <p style={paragraphStyle}>
         This self-criticism deepened during an abusive romantic relationship, further eroding my self-esteem. I fixated on my perceived flaws, especially my nose, believing that changing this one feature would alleviate my feelings of inadequacy.
       </p>
+      <div style = {{ margin : '1rem auto'}}>
+  <AdSenseAd/>
+</div>
 
       <h2 style={headingStyle}>The Decision to Change and Its Consequences</h2>
 
       <p style={paragraphStyle}>
         At 17, fuelled by modest earnings from my YouTube channel and unable to afford rhinoplasty, I went in for an injectable nose job. The procedure was brief, painless, and initially seemed like the perfect solution.
       </p>
+      <div style = {{ margin : '1rem auto'}}>
+  <AdSenseAd/>
+</div>
 
       <p style={paragraphStyle}>
         However, the immediate gratification soon faded, and the deeper issues remained. My insecurities merely shifted as the filler migrated and faded unevenly. Over time, I realized I preferred my original appearance and began to question my perception of myself.
       </p>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img src={image2} alt="Diary Post " style={imageStyle} />
+      <div style={gridContainerStyle}>
+      <img src={image2} alt="Diary Post #1 Jackie Wyers" style={imageStyle} />
       </div>
 
 
@@ -168,39 +230,64 @@ style = {{
       <p style={paragraphStyle}>
         This experience taught me about the complexities of self-esteem and the ineffectiveness of seeking external solutions for internal problems. I learned to cut ties with toxic influences and appreciate my qualities, both physical and personal. Makeup and styling became healthy outlets for creativity, allowing me to express myself in non-permanent ways. My YouTube channel, focusing on pop culture and beauty, has become a source of joy and a platform for fostering a supportive community that builds confidence—not just for me, but for my audience as well.
       </p>
+      <div style = {{ margin : '1rem auto'}}>
+  <AdSenseAd/>
+</div>
 
       <h2 style={headingStyle}>Conclusion: Embracing Authenticity</h2>
 
       <p style={paragraphStyle}>
         My experiences underscore a crucial message: our self-worth shouldn’t hinge solely on our appearance. True healing comes from addressing our deeper emotional needs and respecting ourselves, inside and out.
       </p>
+  
 
       <p style={paragraphStyle}>
         While I recognize that cosmetic enhancements can be positive for some, especially informed adults, I worry about young girls who might view these procedures as solutions to deeper feelings of inadequacy, whether they are aware of their motivations or not. It took years to understand that my feelings of worthlessness were not merely physical insecurities. Making informed decisions about our bodies is challenging during confusing teen and young adult years.
       </p>
+      <div style = {{ margin : '1rem auto'}}>
+  <AdSenseAd/>
+</div>
 
       <p style={paragraphStyle}>
         Through sharing some of my story, I hope to inspire others to reflect on their motivations for physical changes and to embrace their true selves—not just in appearance, but in recognizing how special each personality is and how everyone is worthy of respect and love. I invite you to share your thoughts and reflections below, and I hope you enjoyed my first-ever diary post!
       </p>
 
-      <div style={{ textAlign: 'center', margin: '2rem 0' }}>
-        <img src={signature} alt="Jackie Wyers Signature" style={{ width: '100%', maxWidth: '650px', margin: '0 auto', display: 'block' }} />
+      <div style={gridContainerStyle}>
+      <img src={signature} alt="Jackie Wyers Signature" style={{ width: '100%', maxWidth: '650px', margin: '0 auto', display: 'block' }} />
       </div>
- <div
-style = {{
-  margin : '20px'
-}}
->
 
+      <NextArticle
+      link="/bridesmaidsboxes"
+      imgSrc={bridesmaid}
+      altText="DIY Bridesmaid Boxes"
+      linkText="MORE DIARY POSTS →"
+      containerStyle={{ margin: '1rem 0' }} // customize as needed
+      linkStyle={{ color: '#000000', fontSize: '1.05rem' }} // customize as needed
+      imgStyle={{ borderRadius: '10px' }} // customize as needed
+    />
+
+      <Comments website-id={websiteId} page-id="Diary-Fillers" />
+     
+     
+      <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
-      <Comments website-id={websiteId} page-id="Diary-Fillers" />
+<div style = {{ margin : '1rem auto'}}>
+  <AdSenseAd/>
+</div>
+
     </div>
   );
 
   return (
     <div ref={blogRef}>
-      <Card blogContent={blogContent} />
+      <Card
+        title="Beyond the Surface: My Experience with Injectable Fillers and Self-Acceptance"
+        description="Jackie Wyers shares her personal experience with getting injectable fillers at a young age, reflecting on insecurities, decisions, and growth."
+        blogContent={blogContent}
+      />
+
+
     </div>
   );
 };
