5df8ca805391e6de3cc6ef586df1ea285b961374
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-20 10:10:48 -0400

    43

--- src/Articles/Travel/DropCap1.js
+++ src/Articles/Travel/DropCap1.js
@@ -1,34 +1,22 @@
 import React from 'react';
 
-const DropCap1 = ({ text }) => {
+const DropCap1 = ({ text, style }) => {
   return (
-    <p style={paragraphStyle}>
-      <span style={dropCapStyle}>{text.charAt(0)}</span>
-      {text.slice(1)}
+    <p style={{maxWidth: '500px', margin: '0 auto'}}>
+      <span style={{ 
+        fontSize: '4em', 
+        float: 'left', 
+        margin: '1rem',  
+        lineHeight: '1.5rem', 
+        color: '#745B4F'
+      }}>
+        {text.charAt(0)}
+      </span>
+      <span style={{ lineHeight: '1.5' }}>
+        {text.slice(1)}
+      </span>
     </p>
   );
-};
-
-const dropCapStyle = {
-  fontSize: '5rem', // Size the drop cap large but proportional
-  lineHeight: '0.9', // Tighter line height to align with the top
-  float: 'left', // Allows text to wrap around
-  margin: '1rem 0 0 0', // Adjust margins to control spacing
-  fontFamily: 'GFS Didot, sans-serif',
-  color: '#745B4F', // Accent color for the drop cap
-  fontWeight: '400',
-};
-
-const paragraphStyle = {
-  fontSize: '22px',
-  color: '#000000',
-  margin: '0 auto',
-  fontFamily: 'Gowun Batang, serif',
-  fontWeight: 'normal', // Optional: for a cleaner look
-  maxWidth: '525px',
-  lineHeight: '1.6',
-  margin:'1rem auto',
-  textAlign: 'left', // Optional: for a cleaner look
 };
 
 export default DropCap1;
