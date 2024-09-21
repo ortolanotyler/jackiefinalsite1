4a22e1769accc4c57f1f27bef613060e28900f40
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-21 00:15:56 -0400

    43

--- src/Articles/Tutorials/PopCulture/LanaBlog.js
+++ src/Articles/Tutorials/PopCulture/LanaBlog.js
@@ -1,15 +1,14 @@
 import React, { useRef, useEffect } from 'react';
 import { Comments } from '@hyvor/hyvor-talk-react';
-import '../../ExampleTutorial1.css';
-import TextReveal from '../../../Components/TextReveal';
 import SubscribeButton from '../../../Components/SubscribeButton';
-import DropCap from '../../../Components/DropCap';
 import ResponsiveIframe from '../../../Components/ResponsiveIframe';
 import ResponsiveYoutube from '../../../Components/ResponsiveYoutube';
 import AdSenseAd from '../../../Advertising/Ads';
 import { initGA, logPageView } from '../../../analytics';
 import { Helmet } from 'react-helmet';
 import DropCap1 from '../../Travel/DropCap1';
+import ArticleTitle from '../../../Components/ArticleTitle';
+import NextArticle from '../../../Components/NextArticleComponent';
 
 
 const title = `${process.env.PUBLIC_URL}/Images/Articles/LanaXskims/LanaTitle.png`;
@@ -20,50 +19,105 @@ const Lana23 = `${process.env.PUBLIC_URL}/Images/Articles/Signature.png`;
 const sharon = `${process.env.PUBLIC_URL}/Images/Articles/SharonTateBridal/SHARONTATEBRIDAL.jpeg`;
 
 const Card = ({ blogContent }) => (
-  <div>
-    <div><TextReveal text='POP CULTURE OBSESSED' /></div>
+ 
+  <div style={{
+    padding: '10px 20px', // Consistent padding to prevent content from touching edges
+    margin: '0 auto',
+  }}>
+
     <div className="card">
       <div className="blog-content">{blogContent}</div>
     </div>
-  </div>
+    </div>
+
+  
 );
 
 const LanaBlog = () => {
-  const websiteId = '10910';
-  const blogRef = useRef(null);
 
   useEffect(() => {
     initGA();
     logPageView('/lanablog');
-}, []);
+  }, []);
 
 
+  const websiteId = '10910';
+  const blogRef = useRef(null);
 
-
-const headingStyle = {
-  textAlign: 'center',
-  fontSize: '1.5rem',
-  fontWeight: 'normal',
-  fontFamily: 'Arapey, serif',
-  color: '#333',
-  margin: '1.5rem'
+ 
+  const headingStyle = {
+    textAlign: 'center',
+    fontSize: '2.5rem',
+    fontWeight: 'normal',
+    fontFamily: 'GFS Didot, serif',
+    color: '#000000',
+    margin: '30px auto'
 };
 
+
 const paragraphStyle = {
-  fontSize: '20px',
+  fontSize: '22px',
+  color: '#000000',
+
   fontFamily: 'GFS Didot, serif',
-  margin: '1rem 1.5rem',
-  marginTop: '1rem',
-  marginBottom: '1rem',
-  maxWidth: '500px',
-  lineHeight: '1.5',
+  margin: '2rem auto',
+
+  maxWidth: '500px', // Consistent with the other paragraphs
+  lineHeight: '1.6',
   marginLeft: 'auto',
   marginRight: 'auto',
 };
+
+const linkStyle = {
+  textDecoration: 'none', // Removes underline from links
+  fontFamily: 'GFS Didot, serif',
+  color: '#000000', // Inherits the color of the surrounding text
+};
+
+const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+    gap: '10px',
+    margin: '1rem auto'
+};
+
+const imageStyle = {
+  display: 'block',
+  margin: '1rem auto',
+  borderRadius: '0px',
+  maxWidth: '500px',
+  height: 'auto'
+};
+
+const pinterestGridStyle = {
+  display: 'grid',
+  gap: '1rem',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(236px, 1fr))', // Adjusts columns based on screen size
+  justifyItems: 'center',
+  margin: '1rem 0',
+};
+
+const iframeStyle = {
+  width: '100%',
+  maxWidth: '236px',
+  height: '520px', // Maintains original height
+  border: 'none',
+  scrolling: 'no',
+};
+
+const captionStyle = {
+  textAlign: 'center',
+  fontSize: '1.1rem',
+  fontFamily: 'Playfair Display, serif',
+  margin: '1rem auto',
+  maxWidth: '100%',
+  fontStyle: 'italic',
+};
+
   const blogContent = (
     <div className="container">
     <Helmet>
-    <title>Lana Del Rey SKIMS Makeup Tutorial - Jackie Wyers</title>
+    <title>Recreating SKIMS X Lana Del Rey Photoshoot</title>
     <meta name="description" content="Explore Jackie Wyers' Lana Del Rey SKIMS Makeup Tutorial. Recreate Lana's look with step-by-step guidance, featuring top beauty products and style tips." />
     <meta name="keywords" content="Lana Del Rey, SKIMS, Makeup Tutorial, Jackie Wyers, Beauty Blog, Coquette, 60s Glamour, Romantic Look, Vintage Beauty" />
     <meta property="og:title" content="Lana Del Rey SKIMS Makeup Tutorial - Jackie Wyers" />
@@ -117,39 +171,44 @@ const paragraphStyle = {
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4660168246825318" crossorigin="anonymous"></script>
 </Helmet>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-        <a href="/popculture" style={{ fontFamily: 'Arapey', fontSize: '15px', color: 'black', textDecoration: 'none' }}>
-          BEAUTY // POP CULTURE
+   
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+        <a href="/popculture" style={{ fontFamily: 'GFS Didot, serif', fontSize: '1rem', color: '#000000', textDecoration: 'none' }}>
+        BEAUTY // POP CULTURE
         </a>
       </div>
-      <img src={title} alt="Lana Del Rey X SKIMS" style={{ width: '100%' }} />
 
-      <div style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <SubscribeButton />
-      </div>
+      <ArticleTitle
+  mainTitle="Recreating SKIMS X Lana Del Rey Photoshoot"
+  subTitle="A Coquette Makeup Tutorial for Valentine's Day"
+  author="Jackie Wyers"
+  publishDate="Jan 25th, 2024"
+/>
+
+
+
+
+   
 
       <ResponsiveYoutube src="https://www.youtube.com/embed/9qOTkql3ZHY?si=flnWAs09z00atuiG" title="Lana Del Ray X Skims Makeup Tutorial - Jackie Wyers" />
       
-      <div>
-      <p style={{ margin: '2rem 0',textAlign: 'center', fontSize: '1rem', fontFamily: 'Arapey, sans-serif', maxWidth: '100%', fontStyle: 'italic' }}>
-      All products featured are chosen by Jackie Wyers. We may earn commission on some of the items you choose to buy.
-        </p>
-      </div>
+      <div style = {{textAlign: 'center', margin: '10px auto' }}>
+<SubscribeButton />
+</div>
+
+<p style={{ margin: '20px auto', textAlign: 'center', fontSize: '1.1rem', fontFamily: 'Playfair Display, serif', fontStyle: 'italic' }}>
+All products featured are chosen by Jackie Wyers. We may earn commission on some of the items you choose to buy.
+</p>
 
       <DropCap1 text="Welcome back, Beauties! If your Twitter feed is anything like mine, it's filled with photos from Lana Del Rey's new SKIMS campaign for Valentine's Day, plus memes of little girls praying to a photo of Lana above their bed, worshipping their coquette queen. You've got to love the internet! Seriously though, as a fan of Lana Del Rey's entire discography and aesthetic, I was inspired to recreate the campaign pictures and provide a breakdown of an updated Lana Del Rey makeup look." />
-      <div
-style = {{
-  margin : '20px'
-}}
->
-
+   <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
       <p style={paragraphStyle}>
         Among the various photoshoot looks by photographer Nadia Lee Cohen, my favorite was set against a red backdrop where Lana wore the “Fits Everybody Lace” Slip Dress in Cherry Blossom Tonal from the new SKIMS collection. The dress wasn't out yet at the time I filmed, but you can check out the linked dress below! Plus, the photo set featured kittens, which I also happen to have. Am I a photographer? No, but I’ll try my best to get a vintage-styled photo, and I hope you find some Valentine’s Day inspiration!
       </p>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '2rem 0' }}>
+<div style={gridContainerStyle}>
         <a href="https://www.instagram.com/p/C2vF9bCOVPQ" target="_blank" rel="noopener noreferrer">
           <img src={Lana2} alt="Lana Del Rey Instagram" style={{ width: '100%', maxWidth: '500px' }} />
         </a>
@@ -163,15 +222,10 @@ style = {{
       </p>
 
       <h2 style={headingStyle}>Lana's SKIMS Campaign Makeup Breakdown</h2>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '2rem 0' }}>
+<div style={gridContainerStyle}>
         <img src={lanafacechart} alt='Lana Face Chart' style={{ width: '100%', maxWidth: '55%', height: 'auto' }} />
       </div>
-      <div
-style = {{
-  margin : '20px'
-}}
->
-
+   <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
       <h2 style={headingStyle}>Preparation and Base:</h2>
@@ -185,20 +239,13 @@ style = {{
       </p>
 
       <ResponsiveIframe src="https://shopmy.us/collections/public/493631?noHeader=true" title="Foundation and Concealer Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+
 
       <h2 style={headingStyle}>Contour and Blush:</h2>
       <p style={paragraphStyle}>
         I warmed my skin with a lighter bronzer to emulate Lana's sun-kissed glow. I used Yensa's bronzer, which contains age-defying ingredients, for a natural finish, ensuring it wouldn't crease into fine lines.
       </p>
-      <div
-style = {{
-  margin : '20px'
-}}
->
-
+   <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
       <p style={paragraphStyle}>
@@ -206,9 +253,7 @@ style = {{
       </p>
 
       <ResponsiveIframe src="https://shopmy.us/collections/public/493659?noHeader=true" title="Contour and Blush Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+
 
       <h2 style={headingStyle}>Powder and Highlight:</h2>
       <p style={paragraphStyle}>
@@ -216,15 +261,8 @@ style = {{
       </p>
 
       <ResponsiveIframe src="https://shopmy.us/collections/public/493668?noHeader=true" title="Powder and Highlight Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
-      <div
-style = {{
-  margin : '20px'
-}}
->
 
+   <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
       <h2 style={headingStyle}>Brows and Eye Makeup:</h2>
@@ -236,30 +274,21 @@ style = {{
       </p>
 
       <ResponsiveIframe src="https://shopmy.us/collections/public/493677?noHeader=true" title="Brows and Eye Makeup Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+
 
       <p style={paragraphStyle}>
         A muted, dusty pink appeared to be swept across the lids to add a soft touch of color, complementing the romantic tones of the campaign. For under the lower lash line, I used a rose gold shadow, smoked out near the iris to make the eye color pop. I deepened the lash line again with a deep brown pencil liner in a soft wing and defined the bottom lash line as well. I added a light liner pencil in the waterline for dolly eyes! The eye look was finished with mascara and an outer corner lash in typical Lana fashion.
       </p>
 
       <ResponsiveIframe src="https://shopmy.us/collections/public/493679?noHeader=true" title="Eye Makeup Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+
 
       <h2 style={headingStyle}>Lips:</h2>
       <p style={paragraphStyle}>
         Begin the lip look by over-lining with Makeup Forever Artist Color Pencil in dimensional dark brown. For a blushing ombre effect fading to the deep liner, I chose Amuse Dew Tint in Carrot Dew for the center of the lips. To bring in a bit of warmth, I used Benefit Cosmetics Butterfly blush dabbed from the outer corners of the lips inward to blend the liner and tint together. To capture the essence of Lana's look in the SKIMS campaign, I styled my hair similarly and accessorized with a side pink ribbon. I completed the look with satin gloves and a pink dress that has become a staple in my vintage-inspired wardrobe. You can find the actual SKIMS dress <a href="https://skims.com/products/fits-everybody-lace-slip-dress-cherry-blossom-tonal?irclickid=1MX2vdxOMxyKUSTxnoVKV1VdUkHVsT39rStwwQ0&irgwc=1&utm_medium=affiliate&utm_source=impact&utm_campaign=shopmyshelf" target="_blank" rel="noopener noreferrer" style={{ color: 'black', textDecoration: 'underline' }}>here</a>.
       </p>
 
-      <div
-style = {{
-  margin : '20px'
-}}
->
-
+   <div style = {{ margin : '1rem auto'}}>
   <AdSenseAd/>
 </div>
 
@@ -273,33 +302,32 @@ style = {{
         At the end of my video tutorial, I shared my top picks from the new collection, along with behind-the-scenes with my cats, Lewis and Santana. Here are my top picks from the new line if you're looking to shop. Did you enjoy this campaign shoot as much as I did? Leave a comment below!
       </p>
 
-      <ResponsiveIframe src="https://shopmy.us/collections/embed/370958?noHeader=true" title="SKIMS Collection" />
-      <p style={{ textAlign: 'right', fontSize: '0.75rem', fontFamily: 'GFS Didot, serif', margin: '0.5em 0', maxWidth: '100%' }}>
-        CONTAINS: AFFILIATE LINKS
-      </p>
+      
 
-      <div style={{ textAlign: 'center', margin: '2rem 0' }}>
-        <a href="https://go.shopmy.us/p-3411455" target="_blank" rel="noopener noreferrer">
+      <ResponsiveIframe src="https://shopmy.us/collections/embed/370958?noHeader=true" title="SKIMS Collection" />
+
+
+      <div style={gridContainerStyle}>
+      <a href="https://go.shopmy.us/p-3411455" target="_blank" rel="noopener noreferrer">
           <img src={Lana22} alt="Lana Del Rey" style={{ width: '100%', maxWidth: '500px', display: 'block', margin: '0 auto' }} />
         </a>
       </div>
 
-      <div style={{ textAlign: 'center', margin: '2rem 0' }}>
-        <a href="/sharontatebridal" style={{ fontFamily: 'Arapey', fontSize: '1.25rem', color: 'black', textDecoration: 'underline', textAlign: 'center' }}>
-          <img src={sharon} alt="Sharon Tate Bridal" style={{ width: '100%', maxWidth: '500px', display: 'block', margin: '0 auto' }} />
-          MORE VINTAGE BEAUTY →
-        </a>
-      </div>
+   
 
-      <img src={Lana23} alt="Signature" style={{ width: '100%', display: 'block', margin: '2rem 0' }} />
-      <div
-style = {{
-  margin : '20px'
-}}
->
+      <NextArticle
+      link="/sharontatebridal"
+      imgSrc={sharon}
+      altText="Sharon Tate Bridal"
+      linkText="MORE TIME TRAVEL TUTORIALS →"
+      containerStyle={{ margin: '1rem 0' }} // customize as needed
+      linkStyle={{ color: '#000000', fontSize: '1.05rem' }} // customize as needed
+      imgStyle={{ borderRadius: '10px' }} // customize as needed
+    />
 
-  <AdSenseAd/>
-</div>
+<img src={Lana23} alt="Signature" style={{ width: '100%', display: 'block', margin: '2rem 0' }} />
+
+  
       <Comments website-id={websiteId} page-id={"Lasa-Blog-1aasa"} />
     </div>
   );
@@ -307,8 +335,8 @@ style = {{
   return (
     <div ref={blogRef}>
       <Card
-        title="SKIMS X Lana Del Rey Makeup Tutorial"
-        description="Jackie Wyers' Lana Del Rey SKIMS Makeup Tutorial"
+        title="Recreating SKIMS X Lana Del Rey Photoshoot"
+        description="A Coquette Makeup Tutorial for Valentine's Day"
         blogContent={blogContent}
       />
     </div>
