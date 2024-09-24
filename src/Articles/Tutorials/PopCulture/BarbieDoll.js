0ea0f1850a5e68bbd444475acca72c0e5df1d5fd
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-24 19:01:13 -0400

    43

--- src/Articles/Tutorials/PopCulture/BarbieDoll.js
+++ src/Articles/Tutorials/PopCulture/BarbieDoll.js
@@ -28,14 +28,13 @@ const signature = `${process.env.PUBLIC_URL}/Images/Articles/Signature.png`;
 const Card = ({ blogContent }) => (
   <div
     style={{
-    padding: '1rem',
-      margin: '0 20px',
-      maxWidth: '1600px',
+      padding: '10px 20px', // Existing padding
+      margin: '1rem auto',
+      maxWidth: '800px', // Control max width to center the content
+      padding: '10px', // Add padding to prevent text from touching the edges
     }}
   >
-    <div className="card"
-    
-    >
+    <div className="card">
       <div className="blog-content">{blogContent}</div>
     </div>
   </div>
