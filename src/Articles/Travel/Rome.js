46c57c751aadc95874a3d5aa1bab339b15083299
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-24 14:07:53 -0400

    43

--- src/Articles/Travel/Rome.js
+++ src/Articles/Travel/Rome.js
@@ -70,7 +70,6 @@ const signature = `${process.env.PUBLIC_URL}/Images/Articles/Signature.png`;
 const Card = ({ blogContent }) => (
   <div
     style={{
-      padding: '10px 20px', // Existing padding
       margin: '1rem auto',
       maxWidth: '800px', // Control max width to center the content
       padding: '1.5rem', // Add padding to prevent text from touching the edges
