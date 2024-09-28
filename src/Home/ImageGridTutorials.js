b8f5872967cdc35ab35259898c57159ce26ba3c7
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-28 13:20:13 -0400

    43

--- src/Home/ImageGridTutorials.js
+++ src/Home/ImageGridTutorials.js
@@ -8,14 +8,11 @@ const image2 = `${process.env.PUBLIC_URL}/Images/Home/homepage2.webp`;
 const image3 = `${process.env.PUBLIC_URL}/Images/Home/homepage5.webp`;
 const image4 = `${process.env.PUBLIC_URL}/Images/Home/homepage4.webp`;
 
-
 const itemData = [
   { img: image3, title: 'Jackie Wyers New York City' },
   { img: image1, title: 'Jackie Wyers as Sophia Loren' },
   { img: image4, title: 'Jackie Wyers Hair Tutorials' },
   { img: image2, title: 'Jackie Wyers as Margot Robbie' },
-
-
 ];
 
 export default function QuiltedImageList1() {
@@ -23,32 +20,35 @@ export default function QuiltedImageList1() {
     <ImageList
       sx={{
         marginTop: '-.1rem',
-        width: '100',
+        width: '100%',
         maxHeight: '110vh',
-        overflowY: 'hidden', // Enables scrolling
+        overflowY: 'hidden',
         borderRadius: '1px',
       }}
       variant="quilted"
       cols={4}
-      gap={2} // Adjust gap between images
+      gap={2}
     >
       {itemData.map((item, index) => (
         <ImageListItem
           key={index}
-          cols={1} // Ensures a consistent 4 columns
-          rows={1} // Ensures a consistent 2 rows
+          cols={1}
+          rows={1}
           sx={{
-            overflow: 'hidden', // Hide any overflow from the images
+            overflow: 'hidden',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '300px', // Set a fixed height for all images
           }}
         >
           <img
             src={item.img}
             alt={item.title}
-            loading="lazy"
             style={{
               width: '100%',
               height: '100%',
-              objectFit: 'cover', // Ensure images cover the entire tile without distortion
+              objectFit: 'cover', // Ensures images cover the area without stretching
             }}
           />
         </ImageListItem>
