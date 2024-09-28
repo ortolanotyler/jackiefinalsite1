88391afefa0660a934ec634f269429aeaf1177ee
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-09-27 22:30:18 -0400

    43

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HelmetProvider} from'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <HelmetProvider>
   <React.StrictMode>
