32e350bd6d5d99a5d671edbaee06db9435bc6a16
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-11-17 17:42:00 -0500

    43

--- src/About/About.js
+++ src/About/About.js
@@ -29,29 +29,28 @@ const About = () => {
 
       
       </Grid>
-      <TextReveal text='MEET JACKIE' backgroundColor= 'white'/>
 
       <Grid item xs={12} md={6} className={styles['about-text-section']}>
         <div style={{ padding: '10px', margin: '0 2.5rem', fontFamily: 'Playfair Display, serif', color: '#745B4F' , backgroundColor: '#fdedef'  }} className={styles['about-left-text']}>
-          <p style={{ fontFamily: 'Playfair Display, serif', color: 'black', textAlign: 'center', fontSize: '2.5rem', textDecoration: 'italic' }}>Hello! I'm Jackie Wyers.</p>
+          <p style={{ fontFamily: 'Playfair Display, serif', color: '#3a3a3a', textAlign: 'center', fontSize: '2.5rem', textDecoration: 'italic' }}>Hello! I'm Jackie Wyers.</p>
      
          
-          <p style={{ fontFamily: 'Playfair Display, serif', textIndent: '1rem',fontSize: '1.5rem' , color: '#745B4F', margin: '1rem auto'}}>
+          <p style={{ fontFamily: 'Playfair Display, serif', textIndent: '1rem',fontSize: '1.25rem' , color: '#745B4F', margin: '1rem auto'}}>
             Welcome to where beauty meets pop culture. I started my YouTube career in my teens, and connecting with you through beauty has been a highlight of my life thus far.
           </p>
-          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.5rem' , color: '#745B4F', }}>
+          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.25rem' , color: '#745B4F', }}>
             As a professionally trained makeup artist with a deep passion for character design, I excel at celebrating and recreating iconic looks. Life’s too short not to embrace both new trends and timeless styles. My tutorials are designed to guide you on how to <strong> switch up your style with confidence </strong>.
           </p>
-          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.5rem' , color: '#745B4F', }}>
+          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.25rem' , color: '#745B4F', }}>
             I have a soft spot for TV and films that celebrate sisterhood and style. My comfort TV shows include "H2O: Just Add Water" and "Emily In Paris." Among my favorite films are "Sisterhood of the Traveling Pants," "Mean Girls," "Little Women," and more recently, "The Barbie Movie." I love bringing these characters to life through makeup transformations, celebrating their impact on me and acknowledging the incredible work of the artists behind the scenes.
           </p>
-          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.5rem' , color: '#745B4F', }}>
+          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.25rem' , color: '#745B4F', }}>
             Inspiration also strikes from Taylor Swift's ever-changing eras and the playful 1960s—a transformative era for women with bold makeup trends to match. These inspirations are reflected in my video series, which includes TV and film hairstyles, wearable pop culture makeup transformations, and Time Travel Tutorials that cover all the vintage vibes. You'll also find modern trends and product recommendations here!
           </p>
-          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.5rem' , color: '#745B4F', }}>
+          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.25rem' , color: '#745B4F', }}>
             My goal is to simplify beauty techniques that are practical for your everyday routine while celebrating our unique interests. Whether you're aiming for a Brooke Shields-inspired no-makeup makeup look for a beach day, or a vibrant, pink Barbiecore look reminiscent of Summer 2023, I'm here to help. I aim to make beauty accessible and enjoyable, helping you express yourself effortlessly in your everyday life.
           </p>
-          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.5rem' , color: '#745B4F', }}>
+          <p style={{ fontFamily: 'Playfair Display, serif', fontSize: '1.25rem' , color: '#745B4F', }}>
             Together, we'll create looks that make you feel incredible and pay homage to the iconic moments and styles that inspire us. If you follow my tutorials, tag me on social media—I love seeing your experiments and often reshare them on my channels!
           </p>
           <Box sx={{ textAlign: 'center', marginTop: '2rem' }}>
