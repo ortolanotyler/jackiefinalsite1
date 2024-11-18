0ea0873184de97d0580ad93a828039be11f065eb
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-11-17 22:09:00 -0500

    43

--- src/Navigation/AppBar/ResponsiveAppBar.js
+++ src/Navigation/AppBar/ResponsiveAppBar.js
@@ -17,34 +17,53 @@ const ResponsiveAppBar = () => {
 
   return (
     <>
-     <AppBar
-  position="fixed"
-  sx={{
-    fontFamily: 'Playfair Display, serif',
-    color: '#745B4F',
-    backgroundColor: '#FDEDEF',
-    width: '100%',
-    zIndex: 2300, // Ensure it's higher than the main content
-    boxShadow: 'none',
-  }}
->
+      <AppBar
+        position="fixed"
+        sx={{
+          fontFamily: 'Playfair Display, serif',
+          color: '#745B4F',
+          backgroundColor: '#FDEDEF',
+          width: '100%',
+          zIndex: 2300, // Ensure it's higher than the main content
+          boxShadow: 'none',
+        }}
+      >
         <Container maxWidth="xl" disableGutters>
           <Toolbar
             disableGutters
             sx={{
               justifyContent: 'space-between',
               fontFamily: 'Playfair Display, serif',
-
               alignItems: 'center',
               padding: '5px 10px',
             }}
           >
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              {/* Menu icon for larger screens, positioned to the left of the logo */}
+              {!isCollapsed && (
+                <IconButton
+                  size="large"
+                  aria-label="navigation menu"
+                  onClick={toggleSidebar}
+                  sx={{
+                    color: '#745B4F',
+                    fontFamily: 'Playfair Display, serif',
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                      color: '#745B4F',
+                    },
+                  }}
+                >
+                  <MenuIcon />
+                </IconButton>
+              )}
+              {/* Logo */}
               <Logo toggleSidebar={toggleSidebar} isCollapsed={isCollapsed} />
-
+              {/* Nav buttons for larger screens */}
               {!isCollapsed && <NavButtons />}
             </Box>
 
+            {/* Menu icon for smaller screens */}
             {isCollapsed ? (
               <IconButton
                 size="large"
@@ -53,7 +72,6 @@ const ResponsiveAppBar = () => {
                 sx={{
                   color: '#745B4F',
                   fontFamily: 'Playfair Display, serif',
-
                   '&:hover': {
                     backgroundColor: 'transparent',
                     color: '#745B4F',
@@ -64,29 +82,29 @@ const ResponsiveAppBar = () => {
               </IconButton>
             ) : (
               <Button
-              component={Link}
-              to="/subscribe"
-              sx={{
-                backgroundColor: '#745B4F',
-                color: '#f8f8f8 !important',
-                fontSize: '1rem',
-                fontWeight: '400',
-                textTransform: 'lowercase',
-                letterSpacing: '0.1em',
-                borderRadius: '50px',
-                padding: '10px 20px',
-                fontFamily: 'Playfair Display, serif ',
-                marginRight: '50px',
-                transition: 'color 0.3s ease', // Smooth transition for color change
-                '&:hover': {
-                  color: '#000000 !important', // Change text color on hover
-                  backgroundColor: '#fdedef', // Maintain the same background color
-                  border: '1px solid #745B4F !important'
-                },
-              }}
-            >
-              Subscribe
-            </Button>
+                component={Link}
+                to="/subscribe"
+                sx={{
+                  backgroundColor: '#745B4F',
+                  color: '#f8f8f8 !important',
+                  fontSize: '1rem',
+                  fontWeight: '400',
+                  textTransform: 'lowercase',
+                  letterSpacing: '0.1em',
+                  borderRadius: '50px',
+                  padding: '10px 20px',
+                  fontFamily: 'Playfair Display, serif ',
+                  marginRight: '50px',
+                  transition: 'color 0.3s ease', // Smooth transition for color change
+                  '&:hover': {
+                    color: '#000000 !important', // Change text color on hover
+                    backgroundColor: '#fdedef', // Maintain the same background color
+                    border: '1px solid #745B4F !important',
+                  },
+                }}
+              >
+                Subscribe
+              </Button>
             )}
           </Toolbar>
         </Container>
