0e07781b1260027521ea95dda0d896edc05d85fd
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-11-17 21:50:19 -0500

    43

--- src/Articles/Tutorials/eeebridal.js
+++ src/Articles/Tutorials/eeebridal.js
@@ -18,17 +18,7 @@ const Eeebridal = () => {
             />
           </Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-        <Box sx={{ width: 'auto', height: '100%',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <HeroSection2a
-              featureText="TRAVEL"
-              headlineText="Best of Brussels: Top Things to See, Eat, and Experience!"
-              imagePath="Homepage/paristhumbnail-1.webp"
-              linkUrl="/paris-brussels-travel-guide"
-              isFlipped={true}
-            />
-          </Box>
-        </Grid>
+   
         <Grid item xs={12} sm={6} md={6} lg={4}>
         <Box sx={{ width: 'auto', height: '100%',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <HeroSection2a
