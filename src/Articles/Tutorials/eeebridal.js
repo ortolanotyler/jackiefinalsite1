bc1f235c8eab3fa29361fa29d79769613310006d
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-11-18 21:55:48 -0500

    3

--- src/Articles/Tutorials/eeebridal.js
+++ src/Articles/Tutorials/eeebridal.js
@@ -4,7 +4,7 @@ import HeroSection2a from '../../Home/HeroSection2a';
 
 const Eeebridal = () => {
   return (
-    <Box sx={{ width: '80%', margin: '1rem auto', padding: '30px', backgroundColor: 'white' }}>
+    <Box sx={{ width: '80%', margin: '1rem auto ', padding: '1rem', backgroundColor: 'white' }}>
       <Grid container spacing={2} justifyContent="center">
         {/* Row 1 */}
         <Grid item xs={12} sm={6} md={6} lg={4}>
@@ -39,6 +39,63 @@ const Eeebridal = () => {
               headlineText="The Best Souvenirs to Bring Home from Sorrento"
               imagePath="Homepage/SorrentoThumbnail.webp"
               linkUrl="/sorrento"
+              isFlipped={true}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={4}>
+          <Box
+            sx={{
+              width: 'auto',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <HeroSection2a
+              featureText="DIY"
+              headlineText="How to Make Gorgeous DIY Flower Pins for Your Wedding"
+              imagePath="MammaMia/mammamiathumb.png"
+              linkUrl="/diy-flowers"
+              isFlipped={true}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={4}>
+          <Box
+            sx={{
+              width: 'auto',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <HeroSection2a
+              featureText="BEAUTY"
+              headlineText="Bridal Makeup: How I Created My Dream Wedding Look"
+              imagePath="BridalMakeup/BridalTrialMakeupThumb.jpg"
+              linkUrl="/bridalmakeup"
+              isFlipped={true}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={4}>
+          <Box
+            sx={{
+              width: 'auto',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <HeroSection2a
+              featureText="DIY"
+              headlineText="Bridesmaid Boxes: Whimsical and Elegant Ideas"
+              imagePath="Bridesmaids/BridesmaidBoxThumb!.jpg"
+              linkUrl="/bridesmaidboxes"
               isFlipped={true}
             />
           </Box>
