9d68256d8f5b1cb988088d61a4971f7e4803dc7b
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-12-08 14:15:48 -0500

    43

--- src/Articles/Tutorials/PopCulture/PopCulture.js
+++ src/Articles/Tutorials/PopCulture/PopCulture.js
@@ -11,6 +11,41 @@ const PopCulture = () => {
       <Box sx={{ width: '80%', padding: '1rem', backgroundColor: 'white', margin: '0 auto' }}>
         <Grid container spacing={1} justifyContent="center">
         <Grid item xs={12} sm={6} md={6} lg={4}>
+  <Box
+    sx={{
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <HeroSection2a
+      featureText="FEATURED"
+      headlineText="Taylor Swift Eras Tour Makeup: Recreate Iconic Looks from Lover to Midnights"
+      imagePath="TaylorSwift3/TaylorEvermoreDressonJackie.JPG"
+      linkUrl="/articles/taylor-swift-eras-tour"
+      isFlipped={false}
+    />
+  </Box>
+</Grid>
+        <Grid item xs={12} sm={6} md={6} lg={4}>
+  <Box sx={{ height: '100%', width: '100%' , display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+  
+<HeroSection2a
+  featureText="FEATURED"
+  headlineText="r.e.m. Beauty x WICKED Collection Review: Glinda Makeup Magic"
+  imagePath="Wicked3/GlindaJackieWyers.jpg"
+  linkUrl="/articles/rembeauty/wicked-review"
+  isFlipped={true}
+/>
+
+</Box>
+</Grid>
+
+
+        
+        <Grid item xs={12} sm={6} md={6} lg={4}>
           <Box sx={{ height: '100%', width: '100%' , display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <HeroSection2a
               featureText="TAYLOR : ERAS TOUR 2024"
