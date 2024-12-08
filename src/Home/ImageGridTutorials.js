15ccb6e24940668629598bbb5a4fbf3904d33781
BuildMeAWebsite <contact@buildmeawebsite.ca>
2024-12-07 21:24:48 -0500

    43

--- src/Home/ImageGridTutorials.js
+++ src/Home/ImageGridTutorials.js
@@ -9,21 +9,21 @@ const image3 = `${process.env.PUBLIC_URL}/Images/Home/homepage5.webp`;
 const image4 = `${process.env.PUBLIC_URL}/Images/Home/homepage4.webp`;
 
 const itemData = [
-  { img: image3, title: 'Jackie Wyers New York City' },
-  { img: image1, title: 'Jackie Wyers as Sophia Loren' },
-  { img: image4, title: 'Jackie Wyers Hair Tutorials' },
-  { img: image2, title: 'Jackie Wyers as Margot Robbie' },
+  { img: image3, title: 'Beauty', link: '/beauty' },
+  { img: image1, title: 'Travel', link: '/travel' },
+  { img: image4, title: 'Reviews', link: '/reviews' },
+  { img: image2, title: 'Quiz', link: '/quiz' },
 ];
 
 export default function QuiltedImageList1() {
   return (
     <ImageList
       sx={{
-      
         width: '100%',
         maxHeight: '100vh',
         overflowY: 'hidden',
         borderRadius: '1px',
+        position: 'relative',
       }}
       variant="quilted"
       cols={4}
@@ -39,6 +39,7 @@ export default function QuiltedImageList1() {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            position: 'relative', // Make position relative to allow overlaying
             height: '350px', // Set a fixed height for all images
           }}
         >
@@ -51,6 +52,29 @@ export default function QuiltedImageList1() {
               objectFit: 'cover', // Ensures images cover the area without stretching
             }}
           />
+          <a
+            href={item.link}
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              backgroundColor: 'rgba(0, 0, 0, 0.095)', // Semi-transparent background
+              color: '#ffffff',
+              padding: '1px 20px',
+              borderRadius: '5px',
+              textDecoration: 'none',
+              fontFamily: 'Playfair Display, serif',
+              fontSize: '33px',
+              fontWeight: '400',
+              textAlign: 'center',
+              transition: '0.3s ease',
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.7)')}
+            onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'rgba(0, 0, 0, 0.5)')}
+          >
+            {item.title}
+          </a>
         </ImageListItem>
       ))}
     </ImageList>
